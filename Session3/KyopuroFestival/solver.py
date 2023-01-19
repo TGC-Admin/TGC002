cd2fcc71b0cad80d71ad5f3df8ce3b7bcaef210c
uni_kakurenbo <kohma1116@ybb.ne.jp>
2023-01-19 22:35:42 +0900

    Update: Problem: improve content and sovler

--- Session3/KyopuroFestival/solver.py
+++ Session3/KyopuroFestival/solver.py
@@ -1,76 +1,134 @@
-from collections import deque
+from heapq import heappush, heappop
 from itertools import product
 
-# Dinic's algorithm
-# Thanks to: https://tjkendev.github.io/procon-library/python/max_flow/dinic.html
-class Dinic:
-    def __init__(self, N):
-        self.N = N
-        self.G = [[] for i in range(N)]
-
-    def add_edge(self, fr, to, cap):
-        forward = [to, cap, None]
-        forward[2] = backward = [fr, 0, forward]
-        self.G[fr].append(forward)
-        self.G[to].append(backward)
-
-    def add_multi_edge(self, v1, v2, cap1, cap2):
-        edge1 = [v2, cap1, None]
-        edge1[2] = edge2 = [v1, cap2, edge1]
-        self.G[v1].append(edge1)
-        self.G[v2].append(edge2)
-
-    def bfs(self, s, t):
-        self.level = level = [None]*self.N
-        deq = deque([s])
-        level[s] = 0
-        G = self.G
-        while deq:
-            v = deq.popleft()
-            lv = level[v] + 1
-            for w, cap, _ in G[v]:
-                if cap and level[w] is None:
-                    level[w] = lv
-                    deq.append(w)
-        return level[t] is not None
-
-    def dfs(self, v, t, f):
-        if v == t:
-            return f
-        level = self.level
-        for e in self.it[v]:
-            w, cap, rev = e
-            if cap and level[v] < level[w]:
-                d = self.dfs(w, t, min(f, cap))
-                if d:
-                    e[1] -= d
-                    rev[1] += d
-                    return d
-        return 0
-
-    def flow(self, s, t):
-        flow = 0
-        INF = 10**9 + 7
-        G = self.G
-        while self.bfs(s, t):
-            *self.it, = map(iter, self.G)
-            f = INF
-            while f:
-                f = self.dfs(s, t, INF)
-                flow += f
-        return flow
+# Thanks to: https://github.com/shakayami/ACL-for-python/blob/master/mincostflow.py
+import heapq
+class mcf_graph():
+    n=1
+    pos=[]
+    g=[[]]
+    def __init__(self,N):
+        self.n=N
+        self.pos=[]
+        self.g=[[] for i in range(N)]
+    def add_edge(self,From,To,cap,cost):
+        assert 0<=From and From<self.n
+        assert 0<=To and To<self.n
+        m=len(self.pos)
+        self.pos.append((From,len(self.g[From])))
+        self.g[From].append({"to":To,"rev":len(self.g[To]),"cap":cap,"cost":cost})
+        self.g[To].append({"to":From,"rev":len(self.g[From])-1,"cap":0,"cost":-cost})
+    def get_edge(self,i):
+        m=len(self.pos)
+        assert 0<=i and i<m
+        _e=self.g[self.pos[i][0]][self.pos[i][1]]
+        _re=self.g[_e["to"]][_e["rev"]]
+        return {"from":self.pos[i][0],"to":_e["to"],"cap":_e["cap"]+_re["cap"],
+        "flow":_re["cap"],"cost":_e["cost"]}
+    def edges(self):
+        m=len(self.pos)
+        result=[{} for i in range(m)]
+        for i in range(m):
+            tmp=self.get_edge(i)
+            result[i]["from"]=tmp["from"]
+            result[i]["to"]=tmp["to"]
+            result[i]["cap"]=tmp["cap"]
+            result[i]["flow"]=tmp["flow"]
+            result[i]["cost"]=tmp["cost"]
+        return result
+    def flow(self,s,t,flow_limit=-1-(-1<<63)):
+        return self.slope(s,t,flow_limit)[-1]
+    def slope(self,s,t,flow_limit=-1-(-1<<63)):
+        assert 0<=s and s<self.n
+        assert 0<=t and t<self.n
+        assert s!=t
+        '''
+         variants (C = maxcost):
+         -(n-1)C <= dual[s] <= dual[i] <= dual[t] = 0
+         reduced cost (= e.cost + dual[e.from] - dual[e.to]) >= 0 for all edge
+        '''
+        dual=[0 for i in range(self.n)]
+        dist=[0 for i in range(self.n)]
+        pv=[0 for i in range(self.n)]
+        pe=[0 for i in range(self.n)]
+        vis=[False for i in range(self.n)]
+        def dual_ref():
+            for i in range(self.n):
+                dist[i]=-1-(-1<<63)
+                pv[i]=-1
+                pe[i]=-1
+                vis[i]=False
+            que=[]
+            heapq.heappush(que,(0,s))
+            dist[s]=0
+            while(que):
+                v=heapq.heappop(que)[1]
+                if vis[v]:continue
+                vis[v]=True
+                if v==t:break
+                '''
+                 dist[v] = shortest(s, v) + dual[s] - dual[v]
+                 dist[v] >= 0 (all reduced cost are positive)
+                 dist[v] <= (n-1)C
+                '''
+                for i in range(len(self.g[v])):
+                    e=self.g[v][i]
+                    if vis[e["to"]] or (not(e["cap"])):continue
+                    '''
+                     |-dual[e.to]+dual[v]| <= (n-1)C
+                     cost <= C - -(n-1)C + 0 = nC
+                    '''
+                    cost=e["cost"]-dual[e["to"]]+dual[v]
+                    if dist[e["to"]]-dist[v]>cost:
+                        dist[e["to"]]=dist[v]+cost
+                        pv[e["to"]]=v
+                        pe[e["to"]]=i
+                        heapq.heappush(que,(dist[e["to"]],e["to"]))
+            if not(vis[t]):
+                return False
+            for v in range(self.n):
+                if not(vis[v]):continue
+                dual[v]-=dist[t]-dist[v]
+            return True
+        flow=0
+        cost=0
+        prev_cost=-1
+        result=[(flow,cost)]
+        while(flow<flow_limit):
+            if not(dual_ref()):
+                break
+            c=flow_limit-flow
+            v=t
+            while(v!=s):
+                c=min(c,self.g[pv[v]][pe[v]]["cap"])
+                v=pv[v]
+            v=t
+            while(v!=s):
+                self.g[pv[v]][pe[v]]["cap"]-=c
+                self.g[v][self.g[pv[v]][pe[v]]["rev"]]["cap"]+=c
+                v=pv[v]
+            d=-dual[s]
+            flow+=c
+            cost+=c*d
+            if(prev_cost==d):
+                result.pop()
+            result.append((flow,cost))
+            prev_cost=cost
+        return result
 
 n = int(input())
 T = [[*map(int, input().split())] for _ in [0] * n]
 
-mf_graph = Dinic(2*n+2)
+mf_graph = mcf_graph(2*n+2)
 
 for i in range(n):
-    mf_graph.add_edge(2*n, i, 1)
-    mf_graph.add_edge(n+i, n*2+1, 1)
+    mf_graph.add_edge(2*n, i, 1, 0)
+    mf_graph.add_edge(n+i, n*2+1, 1, 0)
 
 for i, j in product(range(n), range(n)):
     if T[i][0] <= T[j][2] < T[i][1]:
-        mf_graph.add_edge(i, n+j, 1)
+        mf_graph.add_edge(i, n+j, 1, T[i][1] - T[j][2])
 
-print(n - mf_graph.flow(2*n, 2*n+1))
+flow, cost = mf_graph.flow(2*n, 2*n+1);
+
+print(n - flow, cost)
